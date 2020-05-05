41d8296dd239cce2e8403a6249aef772bf78086d
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2020-05-05 10:49:25 +0530

    Add files via upload

---
+++ ALCATRAZ1 - SUM OF DIGITS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{    int t;
+     cin>>t;
+     while(t--){
+       string s;
+       cin>>s;
+       long long sum=0;
+       for(int i=0;i<s.length();i++){
+        sum+=(int)s[i]-'0';
+       }
+       cout<<n<<endl;
+       }
+    return 0;
+}
