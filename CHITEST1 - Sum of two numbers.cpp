8df0adc586d5573b1d666600b38da499d0ab97c6
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2020-05-05 11:28:17 +0530

    Add files via upload

---
+++ CHITEST1 - Sum of two numbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;cin>>t;
+	while(t--)
+	{
+		float m,n;
+		cin>>m>>n;
+		float sum;
+		sum = m+n;
+		cout<<sum<<endl;
+		
+	}
+	return 0;
+}
