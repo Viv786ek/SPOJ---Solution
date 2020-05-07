f95ef1986f1359b775a4e4a40588a9a62bf18883
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2020-05-07 17:54:28 +0530

    Add files via upload

---
+++ SMPWOW - Wow.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int x; cin>>x;
+	cout<<"W";
+	for(int i = 1; i <= x; i++)
+ 	{
+  		cout << "o", i;
+ 	}
+	cout<<"w";
+	return 0;
+}
